e1523214d97fb4664c68c9e21fdc74f00e48d419
Nicholas Rubright <koalrube@gmail.com>
2023-07-26 11:37:39 -0400

    refactored models for better deserialization

--- api/models/post.py
+++ api/models/post.py
@@ -1,7 +1,22 @@
+from marshmallow_generic import GenericSchema, fields
 
-class PostModel:
+
+class Post:
     def __init__(self, id: int, userId: int, title: str, body: str):
         self.id = id
         self.userId = userId
         self.title = title
         self.body = body
+
+    def __str__(self):
+        return f"<Post(id={self.id}, userId={self.userId}, title={self.title})>"
+
+    def __repr__(self):
+        return f"<Post(id={self.id}, userId={self.userId}, title={self.title})>"
+
+
+class PostSchema(GenericSchema[Post]):
+    id = fields.Int()
+    userId = fields.Int()
+    title = fields.Str()
+    body = fields.Str()
---
+++ api/services/jsonapi.py
@@ -0,0 +1,34 @@
+import requests
+from requests import Response
+from typing import List, Any
+from api.models.post import Post, PostSchema
+from marshmallow import ValidationError
+
+
+class ApiError(Exception):
+    pass
+
+
+class JsonApiService:
+    url = "https://jsonplaceholder.typicode.com"
+    postSchema = PostSchema()
+
+    def _checkStatus(self, response: Response) -> Any:
+        if response.status_code > 299 or response.status_code < 200:
+            raise ApiError(f"Status Code: {response.status_code}")
+        return response.json()
+
+    def getPosts(self) -> List[Post]:
+        endpoint = f"{self.url}/posts"
+        r = requests.get(endpoint)
+        try:
+            data = self._checkStatus(r)
+            results = self.postSchema.load(data, many=True)
+        except ApiError as err:
+            print("ApiError: ", err)
+            return []
+        except ValidationError as err:
+            print(err.messages)
+            return []
+
+        return results
